9577de104b54562130beec27f7e305833b1dc49d
chependuk16 <chependuk16@gmail.com>
2023-01-28 00:53:56 +0300

    Initial Commit

---
+++ HelloWorld2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2.01
+//
+//  Created by Мак Сим on 28.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
